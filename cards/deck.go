1f72e6a1a455bb736b1fa2485607ceedc6c7310c
David Riggins <driggins@esri.com>
2023-09-09 18:46:43 -0700

    Lesson 20 End. Custom Type Declarations.

--- cards/deck.go
+++ cards/deck.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+// Create a new type of 'deck' which is a slice of strings
+// It is like the new "deck" type kind of extends the string
+// behavior
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
